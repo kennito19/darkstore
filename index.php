75e7dbd9fa2772bd1dc75dd5b9947cb401efd056
kennito19 <koluoch54@yahoo.com>
2026-09-20 00:35:32 +0300

    Initial store

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DarkStore</title>
+</head>
+<body>
+    <h1>DarkStore</h1>
+    <p>Digital products marketplace</p>
+</body>
+</html>
